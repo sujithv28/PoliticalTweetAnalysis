29c4143bbc07faea44b775b38480f157f036975c
Sujith Vishwajith <sujithv28@gmail.com>
2016-12-06 11:44:59 -0500

    Added Tf-Idf Feature Extraction

--- load_data.py
+++ load_data.py
@@ -10,6 +10,8 @@ import vincent
 from collections import Counter, defaultdict
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Open CSV File to read
 temp_subset_file = open('/Users/sujith/Dropbox/US_UK_ElectionTweets/US_all_tweets/temp_subset.csv')
@@ -63,13 +65,14 @@ def preprocess(s, lowercase=False):
 
 # Keep track of list of tweets, empty Pandas DF,
 # column headers, and frequency counter
-tweets = []
+twitter_data = []
+corpus = []
 data = None
 headers = None
-terms_filtered = Counter()
-terms_all = Counter()
-terms_trump = Counter()
-terms_hillary = Counter()
+terms_filtered = []
+terms_all = []
+terms_trump = []
+terms_hillary = []
 
 i = 0
 for row in csv_f:
@@ -81,51 +84,68 @@ for row in csv_f:
         if len(row[5])>2 and row[5][:2] == 'RT':
             row[5] = row[5][3:]
 
+        unicode_tweet = unicode(row[5], errors='replace')
+        corpus.append(unicode_tweet)
+
         row[5] = [term for term in preprocess(row[5]) if term not in stop]
-        terms_filtered.update([term for term in row[5] if not term.startswith(('#', '@'))])
-        terms_all.update(row[5])
+        terms_filtered.extend([term for term in row[5] if not term.startswith(('#', '@'))])
+        terms_all.extend(row[5])
 
         if 'hillary' in terms_all or 'clinton' in terms_all:
-            terms_hillary.update(terms_all)
+            terms_hillary.extend(terms_all)
         if 'trump' in terms_all or 'donald' in terms_all:
-            terms_trump.update(terms_all)
+            terms_trump.extend(terms_all)
 
-        tweets.append(row)
+        twitter_data.append(row)
     else:
         # Save Column Headers (First row of CSV File)
         headers = row
     i+=1
 
 # Construct Pandas Data Frame from List
-data = pd.DataFrame(tweets, columns=headers)
+data = pd.DataFrame(twitter_data, columns=headers)
 # Count terms only once, equivalent to Document Frequency
 terms_single = set(terms_filtered)
 
 # Print out 10 most frequent words filtered
 print('\nFiltered Frequency:')
-for word, count in terms_filtered.most_common(15):
+terms_filtered_counter = Counter(terms_filtered)
+for word, count in terms_filtered_counter.most_common(15):
     print('{0}: {1}'.format(word, count))
 
 # Print out 10 most unfiltered frequent words
 print('\nUnfiltered Frequency:')
-for word, count in terms_all.most_common(15):
+terms_all_counter = Counter(terms_filtered)
+for word, count in terms_all_counter.most_common(15):
     print('{0}: {1}'.format(word, count))
+
+# Print and generate tfidf matrix from all the tweets
+print('\nTf-Idf Vectors:')
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
+tfidf_matrix =  tf.fit_transform(corpus)
+feature_names = tf.get_feature_names()
+dense = tfidf_matrix.todense()
+densetweets = dense[0].tolist()[0]
+phrase_scores = [pair for pair in zip(range(0, len(densetweets)), densetweets) if pair[1] > 0]
+sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
+for phrase, score in [(feature_names[word_id], score) for (word_id, score) in sorted_phrase_scores][:20]:
+   print('{0: <20} {1}'.format(phrase, score))
 
 print('\n')
 
 # SAVE
-np.save(open(fname, 'w'), data)
-if len(data.shape) == 2:
-    meta = data.index,data.columns
-elif len(data.shape) == 1:
-    meta = (data.index,)
-else:
-    raise ValueError('save_pandas: Cannot save this type')
-s = pickle.dumps(meta)
-s = s.encode('string_escape')
-with open(fname, 'a') as f:
-    f.seek(0, 2)
-    f.write(s)
+# np.save(open(fname, 'w'), data)
+# if len(data.shape) == 2:
+#     meta = data.index,data.columns
+# elif len(data.shape) == 1:
+#     meta = (data.index,)
+# else:
+#     raise ValueError('save_pandas: Cannot save this type')
+# s = pickle.dumps(meta)
+# s = s.encode('string_escape')
+# with open(fname, 'a') as f:
+#     f.seek(0, 2)
+#     f.write(s)
 
 # LOAD
 # values = np.load(fname, mmap_mode=mmap_mode)
